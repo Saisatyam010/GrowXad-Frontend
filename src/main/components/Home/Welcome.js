caf5bfff7bf9a3d23825602374aa1d3a5b74bf75
Deepak <deepak.parashar0248@gmail.com>
2024-03-02 10:59:03 +0530

    resolved code errors

--- src/main/components/Home/Welcome.js
+++ src/main/components/Home/Welcome.js
@@ -7,11 +7,7 @@ import TopSlider from './TopSlider/TopSlider'
 const Welcome = () => {
     return (
         <>
-
-            <section className={`${welcomeStyles.SSA_HOME_parallax}`}>
-                <Container>
-                    <Row className='d-flex align-items-center justify-content-center'>
-                        <Col md={7} className='flex items-center content-center'>
+ 
 
             <section >
                 <Container fluid className={`${welcomeStyles.SSA_HOME_parallax}`}>
@@ -24,10 +20,7 @@ const Welcome = () => {
                                     <p className={`${welcomeStyles.SSA_HOME_21pxFont} py-1 text-white`}>
                                         Empowering Advertising Ecosystems: From Brands to Publishers, Our Network Drives Engagement, Revenue, and Growth with Targeted Strategies and Seamless Integration.
                                     </p>
-                                    {/* Unlock the full potential of your advertising
-                                        campaigns with our cutting-edge platform.
-                                        We are on a mission to empower your brand and extend your influence through revolutionary advertising solutions.
-                                        Our goal is to provide you with the tools and technology needed to elevate your marketing efforts and achieve unparalleled success. */}
+                        
                                     <div class="d-flex align-items-center">
                                         <div className='mr-2 w-40'>
                                             <Link to="/adveriserauthLogin" className='text-black no-underline hover:no-underline border-0 outline-0'> <button type="button" className={`${welcomeStyles.SSA_Btn_HOME_buytraffic} py-3   p-3 w-100`}>BUY TRAFFIC</button></Link>
@@ -40,11 +33,6 @@ const Welcome = () => {
                             </div>
                         </Col>
 
-                        {/* <Col md={5} className=''>
-                            <TopSlider />
-                        </Col> */}
-
-
                         <Col md={6} >
                             <div className='pe-3'>
                                 <img src='https://imagedelivery.net/f5tF3V4WaB6L98qcq1rX5w/8d36c9e1-8cb0-4111-920c-597470549400/public' />
@@ -54,28 +42,6 @@ const Welcome = () => {
                     </Row>
                 </Container>
             </section>
-            {/* <section className={`${welcomeStyles.SSA_HOME_section_background} 
-            ${welcomeStyles.SSA_HOME_parallax} d-flex justify-content-center align-items-center pt-5 text-center`}
-                style={{ overflowX: "hidden" }}>
-                <div>
-                    <div className="row d-flex justify-content-center align-items-center ">
-                        <div className="col-md-7 p-4">
-                            <h1 className={welcomeStyles.heading}>WELCOME TO GrowXAd</h1>
-                            <p className={`${welcomeStyles.SSA_HOME_21pxFont} py-1 p-3`}> Unlock the full potential of your advertising
-                                campaigns with our cutting-edge platform.
-                                We are on a mission to empower your brand and extend your influence through revolutionary advertising solutions.
-                                Our goal is to provide you with the tools and technology needed to elevate your marketing efforts and achieve unparalleled success.</p>
-                        </div>
-                    </div>
-                    <div class="d-flex justify-content-center p-4 align-items-center">
-                        <Link to="/adveriserauthLogin" className='text-black no-underline hover:no-underline border-0 outline-0'> <button type="button" className={`${welcomeStyles.SSA_Btn_HOME_buytraffic} py-3  mr-2 p-3`}>BUY TRAFFIC</button></Link>
-                        <div className='mt-2'>
-                            <Link to="/adveriserauthLogin" className='text-white no-underline hover:no-underline '> <Button variant="outline" className={`${welcomeStyles.gradient_signup}  px-5 mr-2 p-3`}>Sell Traffic</Button>{' '}</Link>
-                        </div>
-                    </div>
-                </div>
-            </section> */}
-
         </>
     )
 }
